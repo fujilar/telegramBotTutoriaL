293895e9efa6c0e54a2efb9537a67ad9e2cde46c
Yuan Zi Lee <yuanzi.lee@global.ntt>
2025-01-04 00:45:46 +0800

    update changes

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from telegram.ext import (Updater, CommandHandler)
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+TELEGRAM_TOKEN=os.getenv("TELEGRAM_TOKEN")
 
 def start_handler(update, context):
     update.message.reply_text("Hello, creater!")
 
 if __name__ == '__main__':
-    updater = Updater("7977898419:AAGXKQrn9xGCGoiCks-sics8zFGxCTidft8", use_context=True)
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler("start", start_handler))
     updater.start_polling()
